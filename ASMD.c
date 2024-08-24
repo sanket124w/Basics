6b71666a42263eebe067e3c62c9a6ad0179a2123
sanket124w <waghmaresanket124@gmail.com>
2024-08-24 16:11:16 +0530

    operations of number

---
+++ ASMD.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main() {
+    //Addition of two number 
+    int a;
+    int b;
+    printf("Enter first number : ");
+    scanf("%d", a);
+
+    printf("Enter second number : ");
+    scanf("%d", b);
+    int suma;
+    suma = a + b;
+    printf("Addition is : %d", suma);
+
+    //Substraction of two number 
+    int c;
+    int d;
+
+    printf("Enter first number : ");
+    scanf("%d", c);
+    printf("Enter second number : ");
+    scanf("%d", d);
+
+    int sums
+    sums = c - d;
+    printf("Substraction is : %d" sums);
+
+    //Multiplication of two number 
+    int e;
+    int f;
+
+    printf("Enter first number : ");
+    scanf("%d", e);
+    printf("Enter second number : ");
+    scanf("%d", f);
+
+    int summ;
+    summ = e * f;
+    printf("Multiplication is : %d", summ);
+
+    //Division of two number 
+    int g;
+    int h;
+
+    printf("Enter frist number : ");
+    scanf("%d", g);
+    printf("Enter second number : ");
+    scanf("%d", h);
+
+    int sumd;
+    sumd = g / h;
+    printf("Division is : %d", sumd);
+    
+
+
+    return 0;
+}
